3609cdc40a403592ad78df0bc1cf9a0098ffd404
Hiroyuki Yamada <mogwaing@gmail.com>
2008-09-04 02:37:57 +0000

    コマンドラインから、文字列と数値を入れるようにした

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "btree.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  LibMap::Btree *bt = new LibMap::Btree;
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " key(string) val(int)" << std::endl; 
+    exit(1);
+  }
 
+  LibMap::Btree *bt = new LibMap::Btree;
   bt->open("test", LibMap::DB_CREAT);
 
   char key[256];
-  uint32_t val = 1980;
   memset(key, 0, 256);
-  strncpy(key, "hello", 5);
+  memcpy(key, argv[1], 256);
+  uint32_t val = atoi(argv[2]);
 
   bt->put(key, strlen(key), &val, sizeof(uint32_t));
   bt->show_node(2);
