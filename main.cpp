277b324ac2092e038952d010a21697e44e274e98
Hiroyuki Yamada <mogwaing@gmail.com>
2008-09-09 03:16:24 +0000

    dbfile名も引数として受け取る

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " key(string) val(int)" << std::endl; 
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0] << " dbname key(string) val(int)" << std::endl; 
     exit(1);
   }
 
   LibMap::Btree *bt = new LibMap::Btree;
-  bt->open("test", LibMap::DB_CREAT);
+  bt->open(argv[1], LibMap::DB_CREAT);
 
   char key[256];
   memset(key, 0, 256);
-  memcpy(key, argv[1], 256);
-  uint32_t val = atoi(argv[2]);
+  memcpy(key, argv[2], 256);
+  uint32_t val = atoi(argv[3]);
 
   bt->put(key, strlen(key), &val, sizeof(uint32_t));
-  bt->show_node();
+  //bt->show_node();
 
   return 0;
 }
