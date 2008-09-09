d4cc71d17894b73df7fbb42127627de5f087c58a
Hiroyuki Yamada <mogwaing@gmail.com>
2008-09-09 13:42:06 +0000

    更新

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 #!/bin/sh 
 
-./main "adam" 100 
-./main "eve" 200 
-./main "duecy" 300 
-./main "btree" 400 
+./main test "adam" 100 && 
+./main test "eve" 200 &&
+./main test "duecy" 300 && 
+./main test "btree" 400 &&
+./main test "dog" 500 &&
+./main test "newyork" 600 &&
+./main test "beck" 700 &&
+./main test "ameba" 800 &&
+./main test "youtube" 900 &&
+./main test "bang" 1000
+./main test "silversun" 1100
 
