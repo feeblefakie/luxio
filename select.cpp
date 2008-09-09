d1bb7cfeb58ee7afc78595c96361980023e566cc
Hiroyuki Yamada <mogwaing@gmail.com>
2008-09-09 13:08:13 +0000

    getテスト用のプログラム

---
+++ select.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "btree.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " dbname key(string)" << std::endl; 
+    exit(1);
+  }
+
+  LibMap::Btree *bt = new LibMap::Btree;
+  bt->open(argv[1], LibMap::DB_CREAT);
+
+  char key[256];
+  memset(key, 0, 256);
+  memcpy(key, argv[2], 256);
+
+  bt->get(key, strlen(key));
+  //bt->show_node();
+
+  return 0;
+}
