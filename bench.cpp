33e6b7a19010d48252251a11372cd81b52348c5a
Hiroyuki Yamada <mogwaing@gmail.com>
2008-09-10 04:28:54 +0000

    benchにselectも追加

--- bench.cpp
+++ bench.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include "btree.h"
+#include <time.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+double gettimeofday_sec()
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + (double)tv.tv_usec*1e-6;
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +22,9 @@ int main(int argc, char *argv[])
   bt->open("benchdb", LibMap::DB_CREAT);
 
   int rnum = atoi(argv[1]);
+  double t1, t2;
 
+  t1 = gettimeofday_sec();
   for (int i = 0; i < rnum; ++i) {
     char key[9];
     memset(key, 0, 9);
@@ -21,7 +33,32 @@ int main(int argc, char *argv[])
 
     bt->put(key, strlen(key), &i, sizeof(uint32_t));
   }
-  //bt->show_node();
+  t2 = gettimeofday_sec();
+  std::cout << "put time: " << t2 - t1 << std::endl;
+  
+  int select_num = rnum / 10;
+  t1 = gettimeofday_sec();
+  for (int i = 0; i < rnum; ++i) {
+    char key[9];
+    memset(key, 0, 9);
+    //sprintf(key,"%08d", i);
+    int num = rand() % rnum;
+    sprintf(key,"%08d", num);
+
+    LibMap::data_t *val_data = bt->get(key, strlen(key));
+    if (val_data != NULL) {
+      uint32_t val;
+      memcpy(&val, val_data->data, val_data->size);
+      if (num != val) {
+        std::cout << "[error] value incorrect." << std::endl;
+      }
+      bt->clean_data(val_data);
+    } else {
+      std::cout << "[error] entry not found." << std::endl;
+    } 
+  }
+  t2 = gettimeofday_sec();
+  std::cout << "put time: " << t2 - t1 << std::endl;
 
   return 0;
 }
