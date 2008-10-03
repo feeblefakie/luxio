2db6020e3f19729b3aae9314b54a1d88a5e566da
Hiroyuki Yamada <mogwaing@gmail.com>
2008-10-03 03:25:57 +0000

    usermem用のコードを追加

--- array_test.cpp
+++ array_test.cpp
@@ -1,5 +1,6 @@
 #include "array.h"
 #include <iostream>
+#include <vector>
 #include <time.h>
 #include <sys/time.h>
 #include <stdio.h>
@@ -32,8 +33,7 @@ int main(int argc, char *argv[])
 
     t1 = gettimeofday_sec();
     for (int i = 0; i < rnum; ++i) {
-      //ary->put(i, &i, sizeof(uint32_t));
-      ary->put2(i, &i, sizeof(uint32_t));
+      ary->put(i, &i, sizeof(uint32_t));
     }
     t2 = gettimeofday_sec();
     std::cout << "put time: " << t2 - t1 << std::endl;
@@ -45,20 +45,35 @@ int main(int argc, char *argv[])
 
   if (mode == 2 || mode == 3) {
 
+    //std::vector<uint32_t> vec;
+    //vec.reserve(rnum);
     t1 = gettimeofday_sec();
     for (int i = 0; i < rnum; ++i) {
       //Lux::DBM::data_t *val_data = ary->get(i);
-      Lux::DBM::data_t *val_data = ary->get2(i);
-      if (val_data != NULL) {
+      Lux::DBM::data_t data;
+      uint32_t val, size;
+      data.data = &val;
+      ary->get(i, &data, &size);
+      //vec.push_back(data->data);
+      // (*(uint32_t *) data.data
+      if (val != i) {
+        std::cout << "[error] value incorrect. i=" << i << ", data=" << val << std::endl;
+      }
+
+      //if (val_data != NULL) {
+        /*
         uint32_t val;
         memcpy(&val, val_data->data, val_data->size);
         if (val != i) {
           std::cout << "[error] value incorrect." << std::endl;
         }
-        ary->clean_data(val_data);
+        */
+        //ary->clean_data(val_data);
+      /*
       } else {
         std::cout << "[error] entry not found. [" << i << "]" << std::endl;
       } 
+      */
     }
     t2 = gettimeofday_sec();
     std::cout << "get time: " << t2 - t1 << std::endl;
