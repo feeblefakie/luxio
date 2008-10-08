290f7adc7b8bdc52974ec7d6e48c7946ee7bb067
Hiroyuki Yamada <mogwaing@gmail.com>
2008-10-08 07:26:05 +0000

    スレッド数を指定できるようにした

--- bench-mt.cpp
+++ bench-mt.cpp
@@ -4,9 +4,9 @@
 #include <sys/time.h>
 #include <stdio.h>
 
-void update_random(int *rnum);
-void write_seq(int *rnum);
-void read_ramdom(int *rnum);
+void update_random(int *num);
+void write_seq(int *num);
+void read_ramdom(int *num);
 
 double gettimeofday_sec()
 {
@@ -15,6 +15,7 @@ double gettimeofday_sec()
   return tv.tv_sec + (double)tv.tv_usec*1e-6;
 }
 
+int rnum;
 int num_recs = 0;
 int num_updated = 0;
 typedef void *(*PROC)(void *);
@@ -22,35 +23,42 @@ Lux::DBM::Btree *bt;
 
 int main(int argc, char *argv[])
 {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " record_num" << std::endl; 
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " record_num thread_num" << std::endl; 
     exit(1);
   }
 
   bt = new Lux::DBM::Btree(Lux::DBM::CLUSTER);
   bt->open("benchdb", Lux::DB_CREAT);
 
-  int rnum = atoi(argv[1]);
+  rnum = atoi(argv[1]);
+  int tnum = atoi(argv[2]);
 
   write_seq(&rnum);
 
-  pthread_t tw, tr1, tr2, tr3, tr4, tr5;
+  pthread_attr_t sched_attr;
+  pthread_attr_init(&sched_attr);
+  //pthread_attr_setschedpolicy(&sched_attr, SCHED_RR);
+  pthread_attr_setscope(&sched_attr, PTHREAD_SCOPE_SYSTEM);
+  pthread_t tw, *thread;
+  thread = new pthread_t[tnum];
   double t1, t2;
 
   t1 = gettimeofday_sec();
   //pthread_create(&tw, NULL, (PROC)update_random, (void *) &rnum);
-  //pthread_create(&tw, NULL, (PROC)write_seq, (void *) &rnum);
-  pthread_create(&tr1, NULL, (PROC)read_ramdom, (void *) &rnum);
-  pthread_create(&tr2, NULL, (PROC)read_ramdom, (void *) &rnum);
-  pthread_create(&tr3, NULL, (PROC)read_ramdom, (void *) &rnum);
-  pthread_create(&tr4, NULL, (PROC)read_ramdom, (void *) &rnum);
-  pthread_create(&tr5, NULL, (PROC)read_ramdom, (void *) &rnum);
+  for (int i = 0; i < tnum; ++i) {
+    //if (pthread_create(&thread[i], &sched_attr, (PROC)read_ramdom, (void *) &i) != 0) {
+    if (pthread_create(&thread[i], NULL, (PROC)read_ramdom, (void *) &i) != 0) {
+      std::cerr << "pthread_create failed" << std::endl;
+    }
+    //pthread_join(thread[i], NULL);
+  }
   //pthread_join(tw, NULL);
-  pthread_join(tr1, NULL);
-  pthread_join(tr2, NULL);
-  pthread_join(tr3, NULL);
-  pthread_join(tr4, NULL);
-  pthread_join(tr5, NULL);
+  ///*
+  for (int i = 0; i < tnum; ++i) {
+    pthread_join(thread[i], NULL);
+  }
+  //*/
   t2 = gettimeofday_sec();
   std::cout << "time(threads): " << t2 - t1 << std::endl;
 
@@ -60,11 +68,11 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-void write_seq(int *rnum)
+void write_seq(int *num)
 {
   double t1, t2;
   t1 = gettimeofday_sec();
-  for (int i = 0; i < *rnum; ++i) {
+  for (int i = 0; i < rnum; ++i) {
     char key[9];
     memset(key, 0, 9);
     sprintf(key,"%08d", i);
@@ -77,14 +85,14 @@ void write_seq(int *rnum)
   std::cout << "put time: " << t2 - t1 << std::endl;
 }
 
-void update_random(int *rnum)
+void update_random(int *num)
 {
   double t1, t2;
   t1 = gettimeofday_sec();
-  for (int i = 0; i < *rnum/10; ++i) {
+  for (int i = 0; i < rnum/10; ++i) {
     char key[9];
     memset(key, 0, 9);
-    int rec_num = (int) random() % *rnum;
+    int rec_num = (int) random() % rnum;
     sprintf(key,"%08d", rec_num);
     //std::cout << "[" << key << "]" << std::endl;
 
@@ -97,11 +105,11 @@ void update_random(int *rnum)
   std::cout << "num_updated: " << num_updated << std::endl;
 }
 
-void read_ramdom(int *rnum)
+void read_ramdom(int *num)
 {
   double t1, t2;
   t1 = gettimeofday_sec();
-  for (int i = 0; i < *rnum; ++i) {
+  for (int i = 0; i < rnum; ++i) {
     char key[9];
     memset(key, 0, 9);
     //sprintf(key,"%08d", i);
@@ -109,6 +117,7 @@ void read_ramdom(int *rnum)
     if (num_recs == 0) { continue; }
     int rec_num = (int) random() % num_recs;
     sprintf(key,"%08d", rec_num);
+    //std::cout << "[" << *num << "] " << key << std::endl;
 
     Lux::DBM::data_t *val_data = bt->get(key, strlen(key));
     if (val_data != NULL) {
@@ -121,9 +130,6 @@ void read_ramdom(int *rnum)
     } else {
       std::cout << "[error] entry not found. [" << key << "]" << std::endl;
     } 
-    if (i % 100000 == 0) {
-      std::cout << i << std::endl;
-    }
   }
   t2 = gettimeofday_sec();
   std::cout << "get time: " << t2 - t1 << std::endl;
