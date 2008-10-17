0a3a867646e8be4596a3f27da56eed708e9a0eca
Hiroyuki Yamada <mogwaing@gmail.com>
2008-10-17 06:58:13 +0000

    テストプログラムの細かい修正  * for multi platform  * put/get selectable

--- ptest/bdb/test.cpp
+++ ptest/bdb/test.cpp
@@ -14,8 +14,8 @@ double gettimeofday_sec()
 }
 int main(int argc, char **argv){
 
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " record_num" << std::endl; 
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " record_num select?" << std::endl; 
     exit(1);
   }
 
@@ -32,77 +32,81 @@ int main(int argc, char **argv){
   flags = DB_CREATE;
 
   dbp->set_cachesize(dbp, 0, 512*1024*1024, 0);
-  ret = dbp->open(dbp, NULL, "my_db.db", NULL, DB_BTREE, flags, 0);
+  ret = dbp->open(dbp, NULL, "./my_db.db", NULL, DB_BTREE, flags, 0);
   if (ret != 0) {
     /* Error handling goes here */
+    fprintf(stderr, "open failed\n");
+    exit(1);
   }
 
   double t1, t2;
   int rnum = atoi(argv[1]);
+  int option = atoi(argv[2]);
 
-  t1 = gettimeofday_sec();
-  for (int i = 0; i < rnum; ++i) {
-    DBT key, data;
-    int ret;
-    char str[9];
-    memset(str, 0, 9);
-    //int num = rand() % rnum;
-    //sprintf(str,"%08d", num);
-    sprintf(str,"%08d", i);
+  if (option == 1 || option == 3) {
+    t1 = gettimeofday_sec();
+    for (int i = 0; i < rnum; ++i) {
+      DBT key, data;
+      int ret;
+      char str[9];
+      memset(str, 0, 9);
+      //int num = rand() % rnum;
+      //sprintf(str,"%08d", num);
+      sprintf(str,"%08d", i);
 
-    /* Zero out the DBTs before using them. */
-    memset(&key, 0, sizeof(DBT));
-    memset(&data, 0, sizeof(DBT));
+      /* Zero out the DBTs before using them. */
+      memset(&key, 0, sizeof(DBT));
+      memset(&data, 0, sizeof(DBT));
 
-    key.data = str;
-    key.size = strlen(str);
-    data.data = &i;
-    data.size = sizeof(int);
+      key.data = str;
+      key.size = strlen(str);
+      data.data = &i;
+      data.size = sizeof(int);
 
-    ret = dbp->put(dbp, NULL, &key, &data, DB_NOOVERWRITE);
-    if (ret == DB_KEYEXIST) {
-      //fprintf(stderr, "duplicate\n");
-    }
-  }
-  t2 = gettimeofday_sec();
-  std::cout << "put time: " << t2 - t1 << std::endl;
-
-/*
-  int select_num = rnum / 10;
-  t1 = gettimeofday_sec();
-  for (int i = 0; i < rnum; ++i) {
-    char str[9];
-    memset(str, 0, 9);
-    //sprintf(key,"%08d", i);
-    int num = rand() % rnum;
-    sprintf(str,"%08d", num);
-
-    DBT key, data;
-    memset(&key, 0, sizeof(DBT));
-    memset(&data, 0, sizeof(DBT));
-
-    key.data = str;
-    key.size = strlen(str);
-
-    uint32_t val;
-    data.data = &val;
-    data.ulen = sizeof(uint32_t);
-    data.flags = DB_DBT_USERMEM;
-    int ret = dbp->get(dbp, NULL, &key, &data, 0);
-
-    if (ret != DB_NOTFOUND) {
-      uint32_t val;
-      memcpy(&val, data.data, data.ulen);
-      if (num != val) {
-        std::cout << "[error] value incorrect." << std::endl;
+      ret = dbp->put(dbp, NULL, &key, &data, DB_NOOVERWRITE);
+      if (ret == DB_KEYEXIST) {
+        //fprintf(stderr, "duplicate\n");
       }
-    } else {
-      std::cout << "[error] entry not found." << std::endl;
-    } 
+    }
+    t2 = gettimeofday_sec();
+    std::cout << "put time: " << t2 - t1 << std::endl;
   }
-  t2 = gettimeofday_sec();
-  std::cout << "get time: " << t2 - t1 << std::endl;
-  */
+
+  if (option == 2 || option ==3) {
+    t1 = gettimeofday_sec();
+    for (int i = 0; i < rnum; ++i) {
+      char str[9];
+      memset(str, 0, 9);
+      //sprintf(key,"%08d", i);
+      int num = rand() % rnum;
+      sprintf(str,"%08d", num);
+
+      DBT key, data;
+      memset(&key, 0, sizeof(DBT));
+      memset(&data, 0, sizeof(DBT));
+
+      key.data = str;
+      key.size = strlen(str);
+
+      uint32_t val;
+      data.data = &val;
+      data.ulen = sizeof(uint32_t);
+      data.flags = DB_DBT_USERMEM;
+      int ret = dbp->get(dbp, NULL, &key, &data, 0);
+
+      if (ret != DB_NOTFOUND) {
+        uint32_t val;
+        memcpy(&val, data.data, data.ulen);
+        if (num != val) {
+          std::cout << "[error] value incorrect." << std::endl;
+        }
+      } else {
+        std::cout << "[error] entry not found." << std::endl;
+      } 
+    }
+    t2 = gettimeofday_sec();
+    std::cout << "get time: " << t2 - t1 << std::endl;
+  }
 
   if (dbp != NULL) {
     dbp->close(dbp, 0); 
--- ptest/tc/test.cpp
+++ ptest/tc/test.cpp
@@ -16,8 +16,8 @@ double gettimeofday_sec()
 }
 int main(int argc, char **argv){
 
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " record_num" << std::endl; 
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " record_num select?" << std::endl; 
     exit(1);
   }
 
@@ -42,50 +42,52 @@ int main(int argc, char **argv){
 
   double t1, t2;
   int rnum = atoi(argv[1]);
+  int option = atoi(argv[2]);
 
-  t1 = gettimeofday_sec();
-  for (int i = 0; i < rnum; ++i) {
-    char key[9];
-    memset(key, 0, 9);
-    //int num = rand() % rnum;
-    //sprintf(key,"%08d", num);
-    sprintf(key,"%08d", i);
-    //std::cout << "[" << key << "]" << std::endl;
-    /* レコードを格納する */
-    //printf("%d\n", i);
-    if(!tcbdbput(bdb, key, strlen(key), &i, sizeof(int))) {
-      fprintf(stderr, "put error\n");
-    }
-  }
-  t2 = gettimeofday_sec();
-  std::cout << "put time: " << t2 - t1 << std::endl;
-  
-  /*
-  int select_num = rnum / 10;
-  t1 = gettimeofday_sec();
-  for (int i = 0; i < rnum; ++i) {
-    char key[9];
-    memset(key, 0, 9);
-    //sprintf(key,"%08d", i);
-    int num = rand() % rnum;
-    sprintf(key,"%08d", num);
-
-    int size;
-    void *data = tcbdbget(bdb, key, strlen(key), &size);
-    if (data != NULL) {
-      uint32_t val;
-      memcpy(&val, data, size);
-      if (num != val) {
-        std::cout << "[error] value incorrect." << std::endl;
+  if (option == 1 || option == 3) {
+    t1 = gettimeofday_sec();
+    for (int i = 0; i < rnum; ++i) {
+      char key[9];
+      memset(key, 0, 9);
+      //int num = rand() % rnum;
+      //sprintf(key,"%08d", num);
+      sprintf(key,"%08d", i);
+      //std::cout << "[" << key << "]" << std::endl;
+      /* レコードを格納する */
+      //printf("%d\n", i);
+      if(!tcbdbput(bdb, key, strlen(key), &i, sizeof(int))) {
+        fprintf(stderr, "put error\n");
       }
-      free(data);
-    } else {
-      std::cout << "[error] entry not found." << std::endl;
-    } 
+    }
+    t2 = gettimeofday_sec();
+    std::cout << "put time: " << t2 - t1 << std::endl;
   }
-  t2 = gettimeofday_sec();
-  std::cout << "get time: " << t2 - t1 << std::endl;
-  */
+  
+  if (option == 2 || option == 3) { 
+    t1 = gettimeofday_sec();
+    for (int i = 0; i < rnum; ++i) {
+      char key[9];
+      memset(key, 0, 9);
+      //sprintf(key,"%08d", i);
+      int num = rand() % rnum;
+      sprintf(key,"%08d", num);
+
+      int size;
+      void *data = tcbdbget(bdb, key, strlen(key), &size);
+      if (data != NULL) {
+        uint32_t val;
+        memcpy(&val, data, size);
+        if (num != val) {
+          std::cout << "[error] value incorrect." << std::endl;
+        }
+        free(data);
+      } else {
+        std::cout << "[error] entry not found." << std::endl;
+      } 
+    }
+    t2 = gettimeofday_sec();
+    std::cout << "get time: " << t2 - t1 << std::endl;
+  }
 
   /* データベースを閉じる */
   if(!tcbdbclose(bdb)){
