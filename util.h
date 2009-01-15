d00afeb6f656c96794f68e7a6fca9952ba52e920
Hiroyuki Yamada <mogwaing@gmail.com>
2009-01-15 08:19:17 +0000

    SAFE_SYSCALLでラップした

--- util.h
+++ util.h
@@ -30,6 +30,23 @@
 #define vinfo_log(msg)
 #endif
 
+// System call wrapper 
+// used in gauche. (macro names are changed.)
+// thanks to shirok and kzk.
+// http://kzk9.net/column/write.html
+#define SAFE_SYSCALL3(result, expr, check) \
+  do { \
+    (result) = (expr); \
+    if ((check) && errno == EINTR) { \
+      errno = 0; \
+    } else { \
+      break; \
+    } \
+  } while (1)
+
+#define SAFE_SYSCALL(result, expr) \
+  SAFE_SYSCALL3(result, expr, (result < 0))
+
 namespace Lux {
 
   static inline void _mkdir(const char *str)
@@ -70,6 +87,27 @@ namespace Lux {
     const char * const end_p = p + count;
 
     while (p < end_p) {
+      int num_bytes;
+      SAFE_SYSCALL(num_bytes, read(fd, p, end_p - p));
+      if (num_bytes < 0) {
+        perror("read failed");
+        break;
+      }
+      p += num_bytes;
+    }
+
+    if (p != end_p) {
+      return -1;
+    }
+    return count;
+  }
+  /*
+  static inline ssize_t _read(int fd, void *buf, size_t count)
+  {
+    char *p = reinterpret_cast<char *>(buf);
+    const char * const end_p = p + count;
+
+    while (p < end_p) {
       const int num_bytes = read(fd, p, end_p - p);
       if (num_bytes < 0) {
         if (errno == EINTR) {
@@ -86,7 +124,29 @@ namespace Lux {
     }
     return count;
   }
+  */
 
+  static inline ssize_t _write(int fd, const void *buf, size_t count)
+  {
+    const char *p = reinterpret_cast<const char *>(buf);
+    const char * const end_p = p + count;
+
+    while (p < end_p) {
+      int num_bytes;
+      SAFE_SYSCALL(num_bytes, write(fd, p, end_p - p));
+      if (num_bytes < 0) {
+        perror("write failed");
+        break;
+      }
+      p += num_bytes;
+    }
+
+    if (p != end_p) {
+      return -1;
+    }
+    return count;
+  }
+  /*
   static inline ssize_t _write(int fd, const void *buf, size_t count)
   {
     const char *p = reinterpret_cast<const char *>(buf);
@@ -107,40 +167,71 @@ namespace Lux {
     }
     return count;
   }
+  */
 
+  static inline bool _pwrite(int fd, const void *buf, size_t nbyte, off_t offset)
+  {
+    const char *p = reinterpret_cast<const char *>(buf);
+    const char * const end_p = p + nbyte;
+
+    while (p < end_p) {
+      int num_bytes;
+      SAFE_SYSCALL(num_bytes, pwrite(fd, p, end_p - p, offset));
+      if (num_bytes < 0) {
+        perror("write failed");
+        break;
+      }
+      p += num_bytes;
+      offset += num_bytes;
+    }
+
+    if (p != end_p) {
+      return false;
+    }
+    return true;
+  }
+  /*
   static inline bool _pwrite(int fd, const void *buf, size_t nbyte, off_t offset)
   {
     if (pwrite(fd, buf, nbyte, offset) < 0) {
       perror("pwrite failed");
       return false;
     }
-    /*
-    if (lseek(fd, offset, SEEK_SET) < 0) {
-      return false;
-    }
-    if (_write(fd, buf, nbyte) < 0) {
-      return false; 
-    }
-    */
     return true;
   }
+*/
 
+  static inline bool _pread(int fd, void *buf, size_t nbyte, off_t offset)
+  {
+    char *p = reinterpret_cast<char *>(buf);
+    const char * const end_p = p + nbyte;
+
+    while (p < end_p) {
+      int num_bytes;
+      SAFE_SYSCALL(num_bytes, pread(fd, p, end_p - p, offset));
+      if (num_bytes < 0) {
+        perror("read failed");
+        break;
+      }
+      p += num_bytes;
+      offset += num_bytes;
+    }
+
+    if (p != end_p) {
+      return false;
+    }
+    return true;
+  }
+  /*
   static inline bool _pread(int fd, void *buf, size_t nbyte, off_t offset)
   {
     if (pread(fd, buf, nbyte, offset) < 0) {
       perror("pread failed");
       return false;
     }
-    /*
-    if (lseek(fd, offset, SEEK_SET) < 0) {
-      return false;
-    }
-    if (_read(fd, buf, nbyte) < 0) {
-      return false; 
-    } 
-    */
     return true;
   }
+  */
 
   static inline void *_mmap(int fd, size_t size, int flags)
   {
